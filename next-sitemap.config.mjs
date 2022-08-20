121f513f157d777557b337a36fc832e8e6d05d28
Hugomndez <hello@hugomendez.dev>
2022-08-19 23:03:30 -0500

    fix: environment url

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const BASE_URL =
+  process.env.NODE_ENV !== 'production'
+    ? process.env.NEXTAUTH_URL
+    : `https://${process.env.VERCEL_URL}`;
+
 const config = {
-  siteUrl: process.env.NEXTAUTH_URL,
+  siteUrl: BASE_URL,
   generateIndexSitemap: false,
   generateRobotsTxt: false,
 };
