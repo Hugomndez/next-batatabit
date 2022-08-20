dc9176cab3ba5068979a61a3d87a1a2e8ceedc1d
Hugomndez <hello@hugomendez.dev>
2022-08-19 23:24:07 -0500

    fix: enviroment test

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
-const BASE_URL =
-  process.env.NODE_ENV !== 'production'
-    ? process.env.NEXTAUTH_URL
-    : `https://${process.env.VERCEL_URL}`;
-
 const config = {
-  siteUrl: BASE_URL,
+  siteUrl: process.env.NEXTAUTH_URL || `https://${process.env.VERCEL_URL}`,
   generateIndexSitemap: false,
   generateRobotsTxt: false,
 };
