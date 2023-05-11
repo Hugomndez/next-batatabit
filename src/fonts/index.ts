aaec5f779c8224b9e983027d0fff7bc329c2a849
Hugomndez <hello@hugomendez.dev>
2023-05-10 19:41:08 -0500

    feat: implemented next/font to avoid CLS

---
+++ src/fonts/index.ts
@@ -0,0 +1,11 @@
+import { DM_Sans, Inter } from 'next/font/google';
+
+export const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+});
+
+export const inter = Inter({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+});
